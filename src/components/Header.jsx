84fbc5f489d783874f4da8db1b2fd7542619ab5c
AmruthaBinu <97900808+AmruthaBinu@users.noreply.github.com>
2026-01-09 09:54:10 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className="header">
-      <h1>Welcome, Arshiya 👋</h1>
+      <h1>Welcome, AMRUTHA 👋</h1>
       <p>Classic Multi-Component React App</p>
     </header>
   );
